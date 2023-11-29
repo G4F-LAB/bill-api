7dd0fca06562acfafe265749db0c862ab30c3b21
M Ninaut <matheus.scheid@g4f.com.br>
2023-11-29 12:38:09 -0300

    added iniats on name

--- app/Models/Collaborator.php
+++ app/Models/Collaborator.php
@@ -4,13 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
 
 class Collaborator extends Model
 {
     use HasFactory;
 
     protected $table = 'collaborators';
-
+    protected $appends = ['name_initials'];
 
     public function permissao() {
         return $this->belongsTo(Permission::class, 'permission_id', 'id');
@@ -20,7 +22,7 @@ class Collaborator extends Model
         $flag = false;
 
         foreach($validPermissions as $index => $validPermission) {
-            
+
             $permissao = Permission::where('name',$validPermission)->first();
             if($permissao == NULL) return response()->json(['error' => 'Permissão inválida: '.$validPermission],404);
 
@@ -44,4 +46,16 @@ class Collaborator extends Model
     public function manager() {
         return $this->hasMany(Contract::class, 'id', 'manager_id');
     }
+
+
+    protected function nameInitials(): Attribute
+    {
+        preg_match('/(?:\w+\. )?(\w+).*?(\w+)(?: \w+\.)?$/', $this->name, $result);
+        $initials =  strtoupper($result[1][0].$result[2][0]);
+
+        return new Attribute(
+            get: fn () => $initials,
+        );
+    }
+
 }
