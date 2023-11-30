b92773ea3e459a5c92400b172939ab4af6bfc211
wesley.severiano <wesley.severiano@g4f.com.br>
2023-11-30 17:31:22 -0300

    Feito ajustes para retorna dos dados do checklist, contrato e itens

--- app/Http/Controllers/ChecklistController.php
+++ app/Http/Controllers/ChecklistController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Checklist;
+use App\Models\FileNaming;
+use App\Models\Contract;
+use App\Models\Item;
 use App\Models\Collaborator;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -20,27 +23,27 @@ class ChecklistController extends Controller
         try{
         $checklist = Checklist::all();
         return response()->json($checklist,200);
-        
+
         }catch(\Exception $e){
             return response()->json(['error'=>'Não foi possivel acessar a checklist'],500);
         }
     }
 
 
-    
+
     public function getById($id){
         try {
             $checklist = Checklist::find($id);
-    
+
             if($checklist) {
 
                 $DataAtual = Carbon::now();
                 $checklistAnteriores = Checklist::orderBy('date_checklist', 'DESC')
                     ->where('date_checklist', '<=', $DataAtual)
                     ->where('id', '!=', $id)
-                    ->limit(3) 
+                    ->limit(3)
                     ->get();
-    
+
                 return response()->json($checklistAnteriores, 200);
             } else {
                 return response()->json(['error' => 'Checklist não encontrado'], 404);
@@ -49,12 +52,12 @@ class ChecklistController extends Controller
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
-    
+
 
     public function store(Request $request)
     {
         // return response()->json([$request->all()],200);
-        
+
         try{
                 $this->checklist->contract_id = $request->contract_id;
                 $this->checklist->date_checklist  = $request->date_checklist;
@@ -66,25 +69,25 @@ class ChecklistController extends Controller
                 $this->checklist->signed_by = $request->signed_by;
                 $this->checklist->save();
                 return response()->json(['message'=>'Checklista criado com sucesso'],200);
-            
-                
+
+
 
         }catch(\Exception $e){
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
-        
+
         public function update(Request $request , $id){
-           try{ 
+           try{
 
             $checklist = Checklist::find($id);
             if(!$checklist){
                 return response()->json(['message'=>'Checklist não existe'],404);
             }
-            
+
             if ($request->method() == 'PATCH') {
                 $dinamicRules = array();
-                
+
                 //aplica regras dinâmicas para os campos que foram enviados
                 foreach ($checklist->rules() as $input => $rule) {
                     if (array_key_exists($input, $request->all())) {
@@ -110,10 +113,32 @@ class ChecklistController extends Controller
             return response()->json(['erro'=> $e->getMessage()],500);
            }
         }
-        
-        
+
+        function checklistItens(Request $request)
+        {
+            try {
+                $id_itens = Item::where('checklist_id', $request->id)->pluck('id');
+
+                if ($id_itens->isEmpty()) {
+                    return response()->json(['error' => 'Items não encontrado.'], 200);
+                }
+
+                $file_names = FileNaming::whereIn('id', $id_itens)->get();
+                $data_checklist = Checklist::where('contract_id', $request->id)->first();
+                $get_contract = Contract::where('id', $data_checklist['id'])->first();
+                $carbonData = Carbon::parse($data_checklist['checklist_date ']);
+                $data['contract'] = $get_contract;
+                $data['contract']['mes_ano'] = $carbonData->format('F/Y');
+                $data['name_itens'] = $file_names;
+                return response()->json($data, 200);
+            } catch (\Exception $exception) {
+                return response()->json(['error' => 'Não foi possível atualizar, tente novamente mais tarde.'], 500);
+            }
+        }
+
+
     }
-    
+
 
 
 
--- routes/api.php
+++ routes/api.php
@@ -65,6 +65,7 @@ Route::middleware(['sec.check', 'handle.cors', 'sys.auth'])->group(function () {
     Route::middleware('check.permission: Admin,Executivo,Operacao,Analista')->put('/checklist/{id}', [ChecklistController::class,'update']);
     Route::middleware('check.permission: Admin,Executivo,Operacao,Analista')->patch('/checklist/{id}', [ChecklistController::class,'update']);
     Route::middleware('check.permission: Admin,Executivo,Operacao,Analista')->post('/checklist/{id}/files', [FileController::class,'uploadChecklistFiles']);
+    Route::middleware('check.permission: Admin,Executivo,Operacao,Analista')->get('/checklist/{id}/items', [ChecklistController::class,'checklistItens']);
 
 
     //Nomenclaturas padrão dos arquivos
