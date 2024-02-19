b01d6964f97c37217d34cc58e6614b21a15887b5
Fernando <fernando.vidal@g4f.com.br>
2024-02-19 15:31:38 -0300

    Criação da migration e da classe de modelo responsável pela tabela de erros da automação

---
+++ app/Models/AutomacaoErrors.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AutomacaoErrors extends Model
+{
+    use HasFactory;
+
+    protected $table = 'automacao_errors';
+}
