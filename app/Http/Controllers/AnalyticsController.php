f7750be841278ece304799d2ccc4ed9ae3e31257
wesley.severiano <wesley.severiano@g4f.com.br>
2024-04-02 15:20:53 -0300

    Correção na parte de "Completion"

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -196,12 +196,15 @@ class AnalyticsController extends Controller
         try {
 
             $id_operation = $request->id;
+            $date = date('Y-m', strtotime('-1 month'));
 
-            $operations = Contract::leftJoin('checklists', 'contracts.id', '=', 'checklists.contract_id')
-            ->select('contracts.id', 'contracts.name', 'checklists.completion')
-            ->where('contracts.status_id', 1)
-            ->where('contracts.operation_id', $id_operation)
-            ->get();
+            $operations = Contract::
+                leftJoin('checklists', 'contracts.id', '=', 'checklists.contract_id')
+                ->select('contracts.id', 'contracts.name', 'checklists.completion')
+                ->where('contracts.status_id', 1)
+                ->where('contracts.operation_id', $id_operation)
+                ->where('date_checklist', 'LIKE', $date . '%')
+                ->get();
 
 
             // $operations = Contract::select('id', 'name')
@@ -219,10 +222,10 @@ class AnalyticsController extends Controller
 
     public function check_complete(Request $request)
     {
-        
-        
+
+
         try {
-            
+
             $operations = $this->getOperationsByUser()->getData();
             $ids_operations = [];
 
