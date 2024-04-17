a2c6ddf4ad7636f5aa32730b1b1836bbc4b5aa1d
talis.santiago <sarkis.paulo@g4f.com.br>
2024-04-17 11:50:47 -0300

    ultimas alterações

--- routes/api.php
+++ routes/api.php
@@ -102,24 +102,7 @@ Route::middleware(['sec.check', 'handle.cors', 'sys.auth'])->group(function () {
         Route::middleware('check.permission: Admin,Executivo,Operacao')->get('/checklistsstatus',[AnalyticsController::class,'qtdStatusChecklists']);
         
         // Route::middleware('check.permission: Admin,Executivo,Operacao')->get('/analytics',[AnalyticsController::class,'getMyAnalytics']);        
-    });
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    });    
 
     Route::middleware('check.permission: Admin')->get('/directory',[DirectoryController::class,'getAnalyticsDirectory']);
     // Route::middleware('check.permission: Admin,Executivo,Operacao')->get('/analytics/{id}',[AnalyticsController::class,'getMyAnalytics']);
@@ -164,7 +147,6 @@ Route::middleware(['sec.check', 'handle.cors', 'sys.auth'])->group(function () {
     //Competencia
     Route::middleware('check.permission: Admin,Executivo,Operacao,Analista,Rh,Fin')->get('/competencias', [FileCompetenceController::class, 'getAll']);
 
-
     //LOG
     Route::middleware('check.permission: Admin,Executivo,Operacao,Analista,Rh,Fin,Geral')->get('/log', [LogController::class, 'show']);
     Route::middleware('check.permission: Admin,Executivo,Operacao,Analista,Rh,Fin,Geral')->post('/log/contract', [LogController::class,'getLogName']);
@@ -172,20 +154,13 @@ Route::middleware(['sec.check', 'handle.cors', 'sys.auth'])->group(function () {
 
     //Automação
     Route::middleware('check.permission: Admin')->post('/automacao', [FileController::class, 'automacao']);
-
     Route::middleware('check.permission:All')->delete('/file/{file_id}/{item_id}', [FileController::class, 'deleteFile']);
 
 });
 
-
 Route::middleware('sys.auth')->get('/teste', [TesteController::class, 'novoteste2']);
 Route::post('/teste', [TesteController::class, 'novoteste']);
 Route::get('/teste2', [TesteController::class, 'novoteste2']);
-
-
-
-
-
 
 Route::get('/documentation', function () {
     return view('documentation');
